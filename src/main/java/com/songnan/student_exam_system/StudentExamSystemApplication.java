c9e74aa67d0d4a054927b6c30fbb49d682bea010
Song <songnan17@qq.com>
2019-04-28 16:26:32 +0800

    Init project.

---
+++ src/main/java/com/songnan/student_exam_system/StudentExamSystemApplication.java
@@ -0,0 +1,13 @@
+package com.songnan.student_exam_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExamSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentExamSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songnan/student_exam_system/StudentExamSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.songnan.student_exam_system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentExamSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
